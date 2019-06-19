b36e9c8d5d586e1d8a25d6e1ef4c8675ede0b7c9
dq-lmt <51999302+dq-lmt@users.noreply.github.com>
2019-06-19 21:48:47 +0800

    上传了Hello.java。
    今天是学程序设计的第一天！我立志要追上王功泽，成为一名Web后端工程师！

---
+++ src/01/Hello.java
@@ -0,0 +1,16 @@
+/*
+一个Java程序就是一系列对象的集合。
+类就是彼此相同的一个模板。例如狗，这一物种，是个类。它有自己的实例变量和方法。
+对象就是类的实例，比如有一直狗叫Ben，那他就是个对象。
+方法（函数）就是一个封装结构——可以打包操作。
+实例变量就是决定对象特征。
+标识符是类或对象的名称。第一个字可以是大写字母、小写字母、下划线、美元中的任意一个、之后的还可以有数字。不能与关键字重名！类则一般都是首字母大写的驼峰命名法。
+命名法中常见的有驼峰命名法和下划线命名法。例如yangYaoHuiShiGeChouBaGuai和yang_yao_hui_shi_ge_chou_ba_guai。
+修饰符可以修饰类中方法和属性。访问性的有default、public、protected、private；非访问性的有final、abstract、static、synchronized。
+变量是一个容器，可以存放值。主要有局部变量、类（静态）变量、成员（动态）变量。
+*/
+public class Hello{
+    public static void main(String[] args
+        System.out.println("Hello, Java World!");
+    }
+}
